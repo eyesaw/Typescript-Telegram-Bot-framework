887dac3f802ee63d14b81475c6e2890eab737b94
eyesaw <eyesaw666@gmail.com>
2018-12-03 23:03:07 +0100

    rework

--- app.ts
+++ app.ts
@@ -1,48 +1,19 @@
-import * as TelegramBot from 'node-telegram-bot-api';
+// require class
+import Bot_Of_Doom from './src/Bot_Of_Doom';
 
-class Bot_Of_Doom
-{
-  readonly token:string = '';
-  readonly bot:any = new TelegramBot( this.token, {polling: true});
+// setup the config
+var config = {
 
-  constructor()
-  {
-    var _ = this;
-
-    this.bot.onText(/\/(.+)/,function (user, match) {
-      _.parse(user,match);
-    });
+  [0]:{
+    token:'tagesgericht',
+    callback: function() {
+      let int = 5;
+      return int;
+    },
+    message: 'kek'
   }
 
-    /**
-     * @param user
-     * @param match (0 - full input / 1 - matching phrase )
-     */
+};
 
-  public parse( user, match )
-  {
-    // ‌‌( match[1].localeCompare('‌tagesgericht') ) === 0
-    switch( match[1] )
-    {
-      case "‌tagesgericht":
-        this.send_message(user, '[*] loading');
-
-        // PDF API generieren
-        // API anfragen
-        // Informationen senden
-
-        this.send_message(user, '[+] done');
-        break;
-
-      default:
-          this.send_message(user, 'invalid argument');
-    }
-  }
-
-  private send_message(user:any, message:string)
-  {
-    this.bot.sendMessage( user.chat.id, message );
-  }
-}
-
-var init = new Bot_Of_Doom();
+// init instance with token
+const bot_of_doom = new Bot_Of_Doom( 'YOUR:::KEY', config );
