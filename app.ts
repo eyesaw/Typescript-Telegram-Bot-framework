f1648dbbd80ce4cfc99e31bb45b90e3d89c59219
eyesaw <eyesaw666@gmail.com>
2018-12-19 21:21:18 +0100

    change compare method

--- app.ts
+++ app.ts
@@ -4,11 +4,11 @@ import Bot_Core from './src/Bot_Core';
 var config = [
   {
     token: 'example',
-    callback: function( user, bot ) {
-      return { chat_id: user.chat.id, text: 'Hello world' };
+    callback: function( user, bot) {
+      bot.sendMessage( user.chat.id, 'Hello world' );
     }
   }
 ];
 
 // init instance with your Telegram API key
-new Bot_Core( 'YOUR::KEY', config );
+new Bot_Core( 'MY::KEY', config );
